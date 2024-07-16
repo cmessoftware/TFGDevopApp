1717fc4a94a160609db5da618388f71e53e9e290
SERGIO SALANITRI <ssalanitri@user.noreply.github.com>
2024-07-16 10:14:25 -0300

    Issue tracking en db

--- TFGDevopsApp/Data/ApplicationDbContext.cs
+++ TFGDevopsApp/Data/ApplicationDbContext.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using TFGDevopsApp.Infraestructure.Entity.Mantis;
+using TFGDevopsApp.Infraestructure.Entity.Plastic;
+using TFGDevopsApp.Infraestructure.Entity.Project;
 
 namespace TFGDevopsApp.Data
 {
@@ -10,5 +12,9 @@ namespace TFGDevopsApp.Data
 
         public DbSet<IssueEntity> Issues { get; set; }
         public DbSet<IssueTracking> IssueTrackings { get;set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Changeset> Changesets { get; set; }
+        public DbSet<ProjectBuild> ProjectBuilders { get; set; }
+
     }
 }
